a77d6dab552de69ce89b72b76a1907fefa9b5bf3
quidama <quidama@naver.com>
2024-01-20 10:35:28 +0900

    change profilemenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,19 @@ import steem from '../../public/steem.png';
 import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 import classNames from '@/services/classNames';
+import ProfileMenu from '@/components/ProfileMenu';
 
 const navigation = [
   { name: '국내문제', tag: 'section-1' },
   { name: '국제문제', tag: 'section-2' },
   { name: '분쟁지역', tag: 'section-3' },
   { name: '참고', tag: 'section-4' },
+];
+
+const profileMenues = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
 ];
 
 export default function Header() {
@@ -92,45 +99,9 @@ export default function Header() {
                     leaveTo='transform opacity-0 scale-95'
                   >
                     <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
+                      {profileMenues?.map((item) => (
+                        <ProfileMenu profileMenu={item} />
+                      ))}
                     </Menu.Items>
                   </Transition>
                 </Menu>
