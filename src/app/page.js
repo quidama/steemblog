7899765b0b7b97c224f3089626b920a8a179bcbc
quidama <quidama@naver.com>
2024-01-20 13:27:25 +0900

    add redirect on home

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
-import Image from 'next/image'
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return <h2>Home</h2>;
+  <span className='sr-obly'>
+    Home 페이지에 내용 넣기 전까지 국내문제로 redirect
+  </span>;
+  redirect('/blogs/section-1');
+  return;
 }
